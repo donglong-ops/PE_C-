963a607f6735faf4d3a383a09f55ac97eb6448ee
donglong-ops <donglong453@gmail.com>
2020-07-17 09:57:59 +0700

    1st upload

---
+++ Department/Department/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Department
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateMrgAss = new System.Windows.Forms.DateTimePicker();
+            this.txtmrgSNN = new System.Windows.Forms.TextBox();
+            this.txtDepName = new System.Windows.Forms.TextBox();
+            this.txtDepNum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateMrgAss);
+            this.groupBox1.Controls.Add(this.txtmrgSNN);
+            this.groupBox1.Controls.Add(this.txtDepName);
+            this.groupBox1.Controls.Add(this.txtDepNum);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(598, 26);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(370, 223);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Deparment Details";
+            // 
+            // dateMrgAss
+            // 
+            this.dateMrgAss.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateMrgAss.Location = new System.Drawing.Point(91, 145);
+            this.dateMrgAss.Margin = new System.Windows.Forms.Padding(2);
+            this.dateMrgAss.Name = "dateMrgAss";
+            this.dateMrgAss.Size = new System.Drawing.Size(226, 20);
+            this.dateMrgAss.TabIndex = 7;
+            // 
+            // txtmrgSNN
+            // 
+            this.txtmrgSNN.Location = new System.Drawing.Point(91, 102);
+            this.txtmrgSNN.Margin = new System.Windows.Forms.Padding(2);
+            this.txtmrgSNN.Name = "txtmrgSNN";
+            this.txtmrgSNN.Size = new System.Drawing.Size(226, 20);
+            this.txtmrgSNN.TabIndex = 6;
+            // 
+            // txtDepName
+            // 
+            this.txtDepName.Location = new System.Drawing.Point(91, 63);
+            this.txtDepName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDepName.Name = "txtDepName";
+            this.txtDepName.Size = new System.Drawing.Size(226, 20);
+            this.txtDepName.TabIndex = 5;
+            // 
+            // txtDepNum
+            // 
+            this.txtDepNum.Location = new System.Drawing.Point(91, 29);
+            this.txtDepNum.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDepNum.Name = "txtDepNum";
+            this.txtDepNum.Size = new System.Drawing.Size(226, 20);
+            this.txtDepNum.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 150);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "mrgAssDate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "mrgSNN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DepName";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DepNum";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(716, 274);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(118, 32);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(847, 274);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(132, 33);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(644, 328);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(130, 33);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgv);
+            this.groupBox2.Location = new System.Drawing.Point(17, 11);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(521, 385);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information Department";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(5, 37);
+            this.dgv.Margin = new System.Windows.Forms.Padding(2);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(512, 259);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(598, 274);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(106, 32);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(826, 328);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(128, 37);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 546);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtmrgSNN;
+        private System.Windows.Forms.TextBox txtDepName;
+        private System.Windows.Forms.TextBox txtDepNum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DateTimePicker dateMrgAss;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Department/Department/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Department
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection;
+        SqlCommand command;
+        String str = "Data Source=FPTU_L;Initial Catalog=FUH_COMPANY;Integrated Security=True";
+       // SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Constr"].ConnectionString);
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+        public void loadData()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select * from tblDepartment";
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dgv.DataSource = table;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(str);
+            connection.Open();
+            loadData();
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "insert into tblDepartment values ('" +txtDepNum.Text+"', '"+txtDepName.Text+"'," +
+                "'"+txtmrgSNN.Text+"', '"+dateMrgAss.Text+"') ";
+            command.ExecuteNonQuery();
+            loadData();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "update tblDepartment set depName = '"+txtDepName.Text+"', mgrSSN = '"+txtmrgSNN.Text+"'," +
+                "mgrAssDate = '"+dateMrgAss.Text+"' where depNum ='"+txtDepNum.Text+"' ";
+            command.ExecuteNonQuery();
+            loadData();
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dgv.CurrentRow.Index;
+            txtDepNum.Text = dgv.Rows[i].Cells[0].Value.ToString();
+            txtDepName.Text = dgv.Rows[i].Cells[1].Value.ToString();
+            txtmrgSNN.Text = dgv.Rows[i].Cells[2].Value.ToString();
+            dateMrgAss.Text = dgv.Rows[i].Cells[3].Value.ToString();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "delete from tblEmployee where depNum ='" + txtDepNum.Text + "'";
+            command.CommandText = "delete from tblDepLocation where depNum ='" + txtDepNum.Text + "'";
+            command.CommandText = "delete from tblDepartment where depNum= '" + txtDepNum.Text + "'";
+
+            command.ExecuteNonQuery();
+            loadData();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtDepNum.Text = "";
+            txtDepName.Text = "";
+            dateMrgAss.Text = "";
+            txtmrgSNN.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
